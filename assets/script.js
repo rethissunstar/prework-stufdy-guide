5fe7840a80410d787214aa0c3ccb09b0151726ff
rethissunstar <rethissunstar@gmail.com>
2023-02-27 14:44:45 -0500

    we have now created a randomizer to work with functions to add information to the log

--- assets/script.js
+++ assets/script.js
@@ -1,19 +1,49 @@
-var topics = ['HTML','CSS','Git','JavaScript'];
+var  topics = ['HTML','CSS','Git','JavaScript'];
+var randomTopic = topics[Math.floor(Math.random() * topics.length)];
 
-for(var x =0; x < topics.length; x++) {
-console.log(topics[x]);
+
+function listtopics(){
+    for(var x =0; x <  topics.length; x++) {
+        console.log( topics[x]);
+        }
 }
+function selecttopics(){
+    if ( randomTopic === 'HTML'){
+        console.log("Let's study HTML");
+    }
+    else if ( randomTopic === 'CSS'){
+        console.log("Let's study CSS");
+    }
+    else if ( randomTopic === 'Git'){
+        console.log("Let's study Git");
+    }
+    else if ( randomTopic === 'JavaScript'){
+        console.log("Let's study JavaScript");
+   
+    }
+    else {
+        console.log("please try again");  
+}
+}
+console.log('here are the topics we learned');
+listtopics();
+console.log('Which should we study first?');
+selecttopics();
 
- /* if (topic === 'HTML'){
+
+//adding the function
+
+
+ /* if ( topics === 'HTML'){
      console.log("Let's study HTML");
  }
- else if (topic === 'CSS'){
+ else if ( topics === 'CSS'){
      console.log("Let's study CSS");
  }
- else if (topic === 'Git'){
+ else if ( topics === 'Git'){
      console.log("Let's study Git");
  }
- else if (topic === 'JavaScript'){
+ else if ( topics === 'JavaScript'){
      console.log("Let's study JavaScript");
 
  }
